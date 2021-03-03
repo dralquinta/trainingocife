619dff0ce3228042b7c2d0beff3abb9f09d119d1
dalquint <denny.alquinta@oracle.com>
2021-03-03 13:18:34 +0000

    Updating entries in setEnv.sh script

--- setEnv.sh
+++ setEnv.sh
@@ -4,9 +4,9 @@
 #Copyright (c) 2021, Oracle and/or its affiliates. All rights reserved. 
 
 # These variables are sent by Jenkins. Upon last commit must be removed/commented out
-export ANSIBLE_WORKSPACE='/home/opc/REPOS/OCIBE/suraansible'
+export ANSIBLE_WORKSPACE='/home/opc/REPOS/OCIBE/trainingansible'
 export TERRAFORM_WORKSPACE='/home/opc/REPOS/OCIFE'
-export PIPELINE='suraocife'
+export PIPELINE='trainingocife'
 export SYSTEM=$1
-export ANSIBLE_BASE_ROLE='base_static_wls_122'
+export ANSIBLE_BASE_ROLE='base_static_sampleconfig'
 export TERRAFORM_ARTIFACT_WORKSPACE=$TERRAFORM_WORKSPACE/$PIPELINE/$SYSTEM
